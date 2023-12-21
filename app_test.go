80ac3d00bbd7c1c455c026e4561ed5d281fc6182
haquenafeem <haquenafeem@gmail.com>
2023-12-21 22:02:26 +0600

    more test added

--- app_test.go
+++ app_test.go
@@ -1,7 +1,10 @@
 package gorocks
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -91,5 +94,95 @@ func TestAddRegexToMap(t *testing.T) {
 	regex := app.routeRegexMap["/*"]
 	if regex.String() != "^\\/[A-Za-z0-9]([A-Za-z0-9_-]*[A-Za-z0-9])?$" {
 		t.Fail()
+	}
+}
+
+func TestHeader(t *testing.T) {
+	app := New()
+	req, err := http.NewRequest(http.MethodGet, "testurl", nil)
+	if err != nil {
+		t.Fail()
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	app.ServeHTTP(nil, req)
+	value := app.Header("Content-Type")
+	if value != "application/json" {
+		t.Fail()
+	}
+}
+
+func TestQuery(t *testing.T) {
+	app := New()
+	app.Post("/testquery", func(a *App) {
+		a.JSON(200, Map{"q1": a.Query("q1"), "q2": a.Query("q2")})
+	})
+
+	req, err := http.NewRequest(http.MethodPost, "/testquery?q1=q1&q2=q2", bytes.NewBuffer([]byte("")))
+	if err != nil {
+		t.Fail()
+	}
+
+	rr := httptest.NewRecorder()
+	app.ServeHTTP(rr, req)
+
+	var m map[string]interface{}
+	body, err := io.ReadAll(rr.Result().Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = json.Unmarshal(body, &m)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if m["q1"] != "q1" || m["q2"] != "q2" {
+		t.Fail()
+	}
+}
+
+func TestBindJSON(t *testing.T) {
+	app := New()
+	app.Post("/testquery", func(a *App) {
+		a.JSON(200, Map{"q1": a.Query("q1"), "q2": a.Query("q2")})
+	})
+
+	req, err := http.NewRequest(http.MethodPost, "/testquery?q1=q1&q2=q2", bytes.NewBuffer([]byte(`{"key":"value"}`)))
+	if err != nil {
+		t.Fail()
+	}
+
+	rr := httptest.NewRecorder()
+	app.ServeHTTP(rr, req)
+
+	m := struct {
+		Key string `json:"key"`
+	}{}
+	err = app.BindJson(&m)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if m.Key != "value" {
+		t.Fail()
+	}
+
+	req, err = http.NewRequest(http.MethodPost, "/testquery?q1=q1&q2=q2", nil)
+	if err != nil {
+		t.Fail()
+	}
+
+	rr = httptest.NewRecorder()
+	app.ServeHTTP(rr, req)
+
+	m = struct {
+		Key string `json:"key"`
+	}{}
+
+	err = app.BindJson(&m)
+	if err != ErrRequestBodyNil {
+		t.Fatal(err)
 	}
 }
